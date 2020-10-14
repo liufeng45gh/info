2b7af9dfdc9d98e98ab5605f49b9a5411e5d4c6c
QXW2194 <Alyson.xianwei.zhang@outlook.com>
2020-10-14 10:16:35 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 
-ps -ef|grep exam-1.0-SNAPSHOT.jar|awk '{print $2}'|xargs kill -9
+ps -ef|grep info-1.0-SNAPSHOT.jar|awk '{print $2}'|xargs kill -9
 
 mvn clean package
 
-cp target/exam-1.0-SNAPSHOT.jar ./exam-1.0-SNAPSHOT.jar
+cp target/info-1.0-SNAPSHOT.jar ./info-1.0-SNAPSHOT.jar
 
-nohup java -jar ./exam-1.0-SNAPSHOT.jar > ./nohup.out &
+nohup java -jar ./info-1.0-SNAPSHOT.jar > ./nohup.out &
 
 
