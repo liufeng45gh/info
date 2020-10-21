a53072b3bea529004fa22e091b372ca0d72868dd
QXW2194 <Alyson.xianwei.zhang@outlook.com>
2020-10-21 18:29:28 +0800

    skip test

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 
 ps -ef|grep info-1.0-SNAPSHOT.jar|awk '{print $2}'|xargs kill -9
 
-mvn clean package
+mvn clean package -Dmaven.test.skip=true
 
 cp target/info-1.0-SNAPSHOT.jar ./info-1.0-SNAPSHOT.jar
 
